b55757ebc7dc1c75c230dc7c24c8443f0a4a74c0
Egor Saplenkov <MiragePR22@gmail.com>
2025-05-14 23:27:30 +0300

    add new function

--- cmd/ration/main.go
+++ cmd/ration/main.go
@@ -2,14 +2,67 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
 	"time"
 
 	"github.com/B0TMirage/week-ration.git/internal/products"
 )
 
 func main() {
+	fmt.Println("Приветствую в приложение \"Счастливый рацион\". Я твой персональный помощник.")
+	time.Sleep(400 * time.Millisecond)
+	fmt.Println("Рацион составляется на неделю по схеме 5-4-3-2-1.")
+	time.Sleep(300 * time.Millisecond)
+	fmt.Println("5 овощей, 4 фрукта, 3 источника белка, 2 крупы, 1 сладость.")
+	time.Sleep(300 * time.Millisecond)
+	fmt.Println("\nКогда будешь готов начать, нажми любую кнопку :З")
+	fmt.Scanln()
+
 	ration := products.GenerateRation()
-	printRation(ration)
+
+	for {
+		fmt.Println("Твой рацион готов: ")
+		printRation(ration)
+
+		fmt.Println("\nВозможные команды: ")
+		var commandNumber int
+		fmt.Println("1. Заменить продукт \n2. Полностью пересмотреть рацион")
+
+	chooseComandLoop:
+		for {
+			fmt.Scanln(&commandNumber)
+
+			switch commandNumber {
+			case 1:
+				var productNumber int
+
+				fmt.Println("\nВведите номер продукта, который хотите заменить")
+				for {
+					fmt.Scanln(&productNumber)
+					if productNumber < 1 || productNumber > 15 {
+						fmt.Println("\nПродукта с данным номером нет в списке")
+						continue
+					}
+					err := products.ReplaceProduct(&ration, productNumber)
+					if err != nil {
+						fmt.Println("\nНевозможно заменить продукт под данным номером, уникальные продукты данной категории закончились")
+						time.Sleep(2 * time.Second)
+					}
+					break chooseComandLoop
+				}
+			case 2:
+				ration = products.GenerateRation()
+				break chooseComandLoop
+			default:
+				fmt.Println("Такой команды не существует")
+				continue
+			}
+		}
+
+		clearConsole()
+	}
 
 }
 
@@ -18,4 +71,15 @@ func printRation(r []string) {
 		fmt.Printf("%d. %s\n", i+1, v)
 		time.Sleep(50 * time.Millisecond)
 	}
+}
+
+func clearConsole() {
+	var cmd *exec.Cmd
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd", "/c", "cls") // Для Windows
+	} else {
+		cmd = exec.Command("clear") // Для Unix-подобных систем
+	}
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 }
--- internal/products/products.go
+++ internal/products/products.go
@@ -1,8 +1,9 @@
 package products
 
 import (
+	"errors"
+	"fmt"
 	"math/rand"
-	"time"
 )
 
 var vegetables, fruits, protein, cereal, sweets []string
@@ -15,19 +16,56 @@ func GenerateRation() []string {
 	for i, numberOfProducts := 0, 5; i < len(availableRation); i, numberOfProducts = i+1, numberOfProducts-1 {
 		currentCategory := availableRation[i]
 		for j := 0; j < numberOfProducts; j++ {
-			randVegIndex := rand.Intn(len(currentCategory))
-			readyRation = append(readyRation, currentCategory[randVegIndex])
-			time.Sleep(10 * time.Millisecond)
-			currentCategory = append(currentCategory[:randVegIndex], currentCategory[randVegIndex+1:]...)
+			readyRation = append(readyRation, getProduct(&currentCategory))
 		}
 	}
 
 	return readyRation
 }
 
+func ReplaceProduct(ration *[]string, index int) error {
+	if index <= 5 {
+		if len(vegetables) <= 1 {
+			return errors.New("not enough objects in the array")
+		}
+		(*ration)[index-1] = getProduct(&vegetables)
+	} else if index <= 9 {
+		if len(fruits) <= 1 {
+			return errors.New("not enough objects in the array")
+		}
+		(*ration)[index-1] = getProduct(&fruits)
+	} else if index <= 12 {
+		if len(protein) <= 1 {
+			return errors.New("not enough objects in the array")
+		}
+		(*ration)[index-1] = getProduct(&protein)
+	} else if index <= 14 {
+		if len(cereal) <= 1 {
+			return errors.New("not enough objects in the array")
+		}
+		(*ration)[index-1] = getProduct(&cereal)
+	} else {
+		fmt.Println(len(sweets))
+		if len(sweets) <= 1 {
+			return errors.New("not enough objects in the array")
+		}
+		(*ration)[index-1] = getProduct(&sweets)
+	}
+
+	return nil
+}
+
+func getProduct(p *[]string) string {
+	i := rand.Intn(len((*p)))
+	product := (*p)[i]
+	(*p) = append((*p)[:i], (*p)[i+1:]...)
+
+	return product
+}
+
 func fillRation() {
-	vegetables = []string{"огурец", "помидоры", "баклажаны", "кабачки", "тыква", "болгарский перец", "брокколи", "цветная капуста", "стручковая фасоль", "морковь", "шпинат", "репчатый лук", "красный лук", "брюссельская капуста", "зелёный горошек", "кукуруза", "спаржа", "редис", "капуста белокочанная", "капуста китайская", "картофель", "краснокачанная капуста", "свекла", "зелёный лук", "укроп", "петрушка", "розмарин", "сельдерей", "кинза", "грибы", "салат айсберг", "авокадо", "батат", "руколу", "цукини", "лук порей"}
-	fruits = []string{"яблоко", "банан", "апельсин", "мандарин", "киви", "груша", "слива", "виноград зелёный", "виноград красный", "голубика", "клубника", "вишня", "малина", "персики", "нектарины"}
+	vegetables = []string{"огурец", "помидоры", "баклажаны", "кабачки", "тыква", "болгарский перец", "брокколи", "цветная капуста", "стручковая фасоль", "морковь", "шпинат", "репчатый лук", "красный лук", "брюссельская капуста", "зелёный горошек", "кукуруза", "спаржа", "редис", "капуста белокочанная", "капуста китайская", "картофель", "краснокачанная капуста", "свекла", "зелёный лук", "укроп", "петрушка", "розмарин", "сельдерей", "кинза", "грибы", "салат айсберг", "авокадо", "батат", "руколу", "цукини", "лук порей", "редька", "репа", "патисоны", "фасоль белая", "фасоль красная", "ламинария", "щавель"}
+	fruits = []string{"яблоко", "банан", "апельсин", "мандарин", "киви", "груша", "слива", "виноград зелёный", "виноград красный", "голубика", "клубника", "вишня", "малина", "персики", "нектарины", "питахайя", "фейхоа", "манго", "папайя", "арбуз", "дыня", "черешня"}
 	protein = []string{"курица", "говядина", "свинина", "кролик", "тунец"}
 	cereal = []string{"рис", "гречка", "булгур"}
 	sweets = []string{"белый шоколад"}
